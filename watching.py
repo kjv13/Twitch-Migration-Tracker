194ba42dbd5ac90a15da68b0e73300affd4387b0
Matthew Roseman <mroseman95@gmail.com>
2016-12-17 04:46:09 +0000

    added better locking in watching.py for the irc and api calls. Also got rid of thread class and just kept update_stream as a method that is added to Thread as a parameter
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

--- watching.py
+++ watching.py
@@ -1,3 +1,4 @@
+# import pdb
 import time
 import threading
 from irc_connect import IRCConnection
@@ -24,116 +25,117 @@ leave_ttl = 300
 # the result is double checked with an API call
 irc_min_users = 100
 
-print_lock = threading.Lock()
-irc_lock = threading.Lock()
-api_lock = threading.Lock()
+print_lock = threading.RLock()
+irc_lock = threading.RLock()
+api_lock = threading.RLock()
 
 
-class update_thread (threading.Thread):
+def update_stream(stream):
+    with print_lock:
+        print('Entering thread: ' + stream)
+    users = get_users(stream)
+    #  if the users list is empty something wen't wrong
+    if len(users) == 0:
+        return
 
-    def __init__(self, streamname, name=None):
-        super().__init__(name=name)
-        self.stream = streamname
+    users_joining = []
+    users_leaving = []
+    #  really these are an array of json objects
+    joining_json = []
+    leaving_json = []
 
-    def run(self):
-        users = get_users(self.stream)
-        #  if the users list is empty something wen't wrong
-        if len(users) == 0:
-            return
-
-        users_joining = []
-        users_leaving = []
-        #  really these are an array of json objects
-        joining_json = []
-        leaving_json = []
-
-        # figure out any new users and any users that have left
-        old_users = con.db[con.watching_collection].find(
-                {
-                    # find all documents with 'streamname' = stream
-                    'streamname': self.stream
-                },
-                {
-                    # project only the watching field
-                    '_id': False,
-                    'watching': True
-                }
-        )
-        #  if this stream is in the database get joining and leaving users
-        if old_users.count() == 1:
-            old_users = set(old_users[0]['watching'])
-            new_users = set(users)
-            #  users in new_users that aren't in old_users
-            users_joining = new_users - old_users
-            #  users in old_users that are no longer in new_users
-            users_leaving = old_users - new_users
-
-        # with print_lock:
-        #     print('\njoining users in {0}: {1}'.format(self.stream,
-        #           users_joining))
-        #     print('leaving users in {0}: {1}\n'.format(self.stream,
-        #           users_leaving))
-
-        #  create the new joining and leave json to add to the database
-        for user in users_joining:
-            joining_json.append(
-                {
-                    'username': user,
-                    'last_updated': time.time()
-                }
-            )
-        for user in users_leaving:
-            leaving_json.append(
-                {
-                    'username': user,
-                    'last_updated': time.time()
-                }
-            )
-
-        #  add these users to this streams document
-        #  TODO make sure the joining and leaving json appends to the list and
-        #  that any elements that have expiredd are removed
-
-        # with print_lock:
-        #     print('updating watching users in the database')
-
-        con.db[con.watching_collection].update_one(
-            {'streamname': self.stream},
+    # figure out any new users and any users that have left
+    old_users = con.db[con.watching_collection].find(
             {
-                '$set': {
-                    'watching': users,
-                },
-                '$push': {
-                    'joining': {
-                        '$each': joining_json,
-                    },
-                    'leaving': {
-                        '$each': leaving_json
-                    }
-                },
-                '$setOnInsert': {
-                    'streamname': self.stream,
-                }
+                # find all documents with 'streamname' = stream
+                'streamname': stream
+            },
+            {
+                # project only the watching field
+                '_id': False,
+                'watching': True
+            }
+    )
+    #  if this stream is in the database get joining and leaving users
+    if old_users.count() == 1:
+        old_users = set(old_users[0]['watching'])
+        new_users = set(users)
+        #  users in new_users that aren't in old_users
+        users_joining = new_users - old_users
+        #  users in old_users that are no longer in new_users
+        users_leaving = old_users - new_users
+
+    # with print_lock:
+    #     print('\njoining users in {0}: {1}'.format(self.stream,
+    #           users_joining))
+    #     print('leaving users in {0}: {1}\n'.format(self.stream,
+    #           users_leaving))
+
+    #  create the new joining and leave json to add to the database
+    for user in users_joining:
+        joining_json.append(
+            {
+                'username': user,
+                'last_updated': time.time()
             }
         )
-        #  removing any stale joining or leaving users
-        con.db[con.watching_collection].update_one(
-            {'streamname': self.stream},
+    for user in users_leaving:
+        leaving_json.append(
             {
-                '$pull': {
-                    'joining': {
-                        'last_updated': {
-                            '$lte': time.time() - join_ttl
-                        }
-                    },
-                    'leaving': {
-                        'last_updated': {
-                            '$lte': time.time() - leave_ttl
-                        }
+                'username': user,
+                'last_updated': time.time()
+            }
+        )
+
+    #  add these users to this streams document
+    #  TODO make sure the joining and leaving json appends to the list and
+    #  that any elements that have expiredd are removed
+
+    # with print_lock:
+    #     print('updating watching users in the database')
+
+    con.db[con.watching_collection].update_one(
+        {'streamname': stream},
+        {
+            '$set': {
+                'watching': users,
+            },
+            '$push': {
+                'joining': {
+                    '$each': joining_json,
+                },
+                'leaving': {
+                    '$each': leaving_json
+                }
+            },
+            '$setOnInsert': {
+                'streamname': stream,
+            }
+        },
+        True
+    )
+    #  removing any stale joining or leaving users
+    con.db[con.watching_collection].update_one(
+        {'streamname': stream},
+        {
+            '$pull': {
+                'joining': {
+                    'last_updated': {
+                        '$lte': time.time() - join_ttl
+                    }
+                },
+                'leaving': {
+                    'last_updated': {
+                        '$lte': time.time() - leave_ttl
                     }
                 }
             }
-        )
+        },
+        True
+    )
+    with print_lock:
+        print('Exiting thread: ' + stream)
+        print('\n\n{}\n\n'.format(threading.active_count()))
 
 
 def get_users(channel):
@@ -147,11 +149,11 @@ def get_users(channel):
     global api_lock
 
     with irc_lock:
-        print(threading.current_thread().name + ' getting users for: ' +
-              channel)
+        # print(threading.current_thread().name + ' getting users for: ' +
+        #       channel)
         users = irc.get_channel_users(channel)
-        print((threading.current_thread().name + ' length of users gotten ' +
-              'from IRC is {0}').format(len(users)))
+        # print((threading.current_thread().name + ' length of users gotten ' +
+        #       'from IRC is {0}').format(len(users)))
 
     #  print random string to make reading terminal easier
     # print(''.join(random.choice(string.ascii_uppercase + string.digits) for _
@@ -159,8 +161,8 @@ def get_users(channel):
 
     if len(users) < irc_min_users:
         with api_lock:
-            print(('IRC user count is less than {0}, now double checking ' +
-                  'with API call').format(irc_min_users))
+            # print(('IRC user count is less than {0}, now double checking ' +
+            #       'with API call').format(irc_min_users))
             users = api.get_users(channel)
 
     return users
@@ -202,9 +204,16 @@ while True:
             }
     )
 
+    threads = []
+    print('\n\n{}\n\n'.format(threading.active_count()))
     for stream in streams:
-        t = update_thread(stream, stream+'-thread')
-        t.daemon = True
+        t = threading.Thread(target=update_stream, name=stream+'-thread',
+                             args=(stream,))
+        print('{} threads created'.format(len(streams)))
+        threads.append(t)
         t.start()
-    while threading.active_count() > 0:
-        continue
+
+    for t in threads:
+        t.join()
+        print('\n\nthread joined back into main thread\n\n')
+    print('all threads joined back into main thread')
