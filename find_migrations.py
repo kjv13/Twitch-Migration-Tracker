cdd598a1481e940b2084574822e6c1c3d0309f0d
Matthew Roseman <mroseman95@gmail.com>
2016-12-17 12:46:19 +0000

    successfully found users who left streams and went to another stream. Stil not loggin to database. Also taking advantage of pythons hash tables in dicts to effectively process the usernames and last_updated data
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

--- find_migrations.py
+++ find_migrations.py
@@ -1,4 +1,3 @@
-import time
 from db_connect import NoSQLConnection
 
 print('connection to noSQL database')
@@ -31,29 +30,54 @@ streams = con.db[con.watching_collection].find(
 # ('streamname',
 # [('username','last_updated'),...],
 # [('username','last_updated'),...])
+# streams = list(map(lambda s: (s['streamname'],
+#                               (list(map(lambda j: (j['username'],
+#                                                   j['last_updated']),
+#                                        s['joining'])),
+#                               list(map(lambda l: (l['username'],
+#                                                   l['last_updated']),
+#                                        s['leaving']))),
+#                    streams))
+
+# this should create a tuple of the form ...
+# ('streamname', {'username': 'last_updated',...},
+# {'username': 'last_updated',...})
+# where the first value is a single string representing the stream name
+# the second value is a dict representing users joining
+# and the third value is a dict representing users leaving
+# the dict is of the form ...
+# {
+#  'username': 'last_updated',
+#  ...
+# }
 streams = list(map(lambda s: (s['streamname'],
-                              list(map(lambda j: (j['username'],
-                                                  j['last_updated']),
-                                       s['joining'])),
-                              list(map(lambda l: (l['username'],
-                                                  l['last_updated']),
-                                       s['leaving']))),
+                              dict(zip(list(map(lambda j: j['username'],
+                                                s['joining'])),
+                                       list(map(lambda j: j['last_updated'],
+                                                s['joining'])))),
+                              dict(zip(list(map(lambda l: l['username'],
+                                                s['leaving'])),
+                                       list(map(lambda l: l['last_updated'],
+                                                s['leaving']))))),
                    streams))
 
 # for every pair of streams
-for i in streams:
+for l in streams:
     for j in streams:
-        # get the list of users leaving i
-        leaving_i = i[2]
-        # list of users joining j
-        joining_j = j[1]
-        # for every pair of leaving and joining users
-        for l in leaving_i:
-            for j in joining_j:
-                # if the leaving user and joining user has the same username
-                if l[0] == j[0]:
-                    # find the difference and time and see if its less than
-                    # limit
-                    if max(l[1], j[1]) - min(l[1], j[1]) <= related_limit:
-                        # this is considered a connection so add it to db
-                        pass
+        # if i and j are the same stream we don't care about leaving and
+        # joining
+        if l[0] == j[0]:
+            continue
+        # for every user that joined i and left j
+        # user = the strings of the keys that are found in i and j
+        # where the keys are the usernames of users who leave/join
+        for user in set(l[2].keys() & j[1].keys()):
+            # find the difference and time and see if its less than
+            # limit
+            # i[user] gets the value for the key user, which is the
+            # last_updated time
+            if (max(l[2][user], j[1][user]) -
+                    min(l[2][user], j[1][user]) <= related_limit):
+                # this is considered a connection so add it to db
+                print(('user {0} left stream {1} and went to stream ' +
+                      ' {2}').format(user, l[0], j[0]))
